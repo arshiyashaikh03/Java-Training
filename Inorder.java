0588622ca9b2963bdad9e327715af79f90b38b2f
arshiyashaikh03 <158029888+arshiyashaikh03@users.noreply.github.com>
2026-06-08 14:28:57 +0530

    Add files via upload

---
+++ Inorder.java
@@ -0,0 +1,39 @@
+public class Inorder {
+
+    static class TreeNode {
+        int key;
+        TreeNode left, right;
+
+        TreeNode(int key) {
+            this.key = key;
+            left = right = null;
+        }
+    }
+
+    static void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.key + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(10);
+
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(15);
+
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(9);
+
+        root.left.right.left = new TreeNode(7);
+        root.left.right.left.left = new TreeNode(4);
+
+        System.out.println("Inorder Traversal:");
+        inorder(root);
+    }
+}
